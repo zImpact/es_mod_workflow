2afa124791b88502cbb4b98cdbc53acf21946b5d
Andrey Kataev <67541326+paych3ck@users.noreply.github.com>
2025-03-03 20:04:17 +0300

    Обновление проверки статуса джобы
    Проверка статуса джобы basic-lint

--- check_lint_status.sh
+++ check_lint_status.sh
@@ -1,27 +1,42 @@
 #!/bin/bash
-# check_lint_status.sh
-# Использование: ./check_lint_status.sh <workflow_file> <branch>
-# Скрипт получает статус последнего запуска указанного workflow на заданной ветке.
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <workflow_file> <branch>"
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <workflow_file> <branch> <job_name>"
   exit 1
 fi
 
 WORKFLOW_FILE=$1
 BRANCH=$2
+JOB_NAME=$3
+
 REPO=${GITHUB_REPOSITORY}
 GITHUB_TOKEN=${GITHUB_TOKEN}
 
-echo "Проверяем последний запуск workflow '${WORKFLOW_FILE}' для ветки '${BRANCH}' в репозитории '${REPO}'..."
+echo "Проверяем статус job '${JOB_NAME}' в workflow '${WORKFLOW_FILE}' для ветки '${BRANCH}' в репозитории '${REPO}'..."
 
-response=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
+workflow_run_response=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
   "https://api.github.com/repos/${REPO}/actions/workflows/${WORKFLOW_FILE}/runs?branch=${BRANCH}&per_page=1")
+run_id=$(echo "$workflow_run_response" | jq -r '.workflow_runs[0].id')
 
-conclusion=$(echo "$response" | jq -r '.workflow_runs[0].conclusion')
+if [ -z "$run_id" ] || [ "$run_id" == "null" ]; then
+  echo "Не найден последний запуск workflow '${WORKFLOW_FILE}' для ветки '${BRANCH}'"
+  exit 1
+fi
 
-echo "Статус последнего запуска: $conclusion"
-if [ "$conclusion" != "success" ]; then
-  echo "Lint workflow не прошёл успешно. Деплой прерван."
+echo "ID последнего запуска workflow: $run_id"
+
+jobs_response=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
+  "https://api.github.com/repos/${REPO}/actions/runs/${run_id}/jobs")
+
+job_conclusion=$(echo "$jobs_response" | jq -r --arg JOB_NAME "$JOB_NAME" '.jobs[] | select(.name==$JOB_NAME) | .conclusion')
+
+if [ -z "$job_conclusion" ] || [ "$job_conclusion" == "null" ]; then
+  echo "Job с именем '${JOB_NAME}' не найден в запуске ${run_id}"
+  exit 1
+fi
+
+echo "Результат job '${JOB_NAME}': $job_conclusion"
+if [ "$job_conclusion" != "success" ]; then
+  echo "Job '${JOB_NAME}' не завершился успешно. Деплой прерван."
   exit 1
 fi
